59303a940ee9aaa3bf7ece522d4b231109fe58a7
Kevin Jessie <kevinjessie32@yahoo.com>
2023-05-23 18:41:11 -0700

    update

---
+++ Develop/README.md
@@ -0,0 +1,46 @@
+# ReadMe
+  ![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description
+  A professional readme made by me.
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [License](#license)
+  * [License
+  * [Questions](#questions)
+  * [Contributing](#contributing)
+  
+  ## Installation
+  Open the Develop app terminal and run node index inquirer
+  ## Usage
+  To generate a readme
+  ## Credits
+  none
+  ## Tests
+  To run the app in node index inquirer
+  ## License
+  MIT License
+  https://opensource.org/licenses/MIT
+  ## Questions
+  none
+  ## Contributing
+  none
+
+  ## Contact
+  * GitHub: [KevinJessie](
+  
+
+  ## Questions
+
+  If you have any questions, please contact me at kevinjessie32@yahoo.com
+
+  ## License
+
+  This project is licensed under the MIT license.
+
+  
+
+
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const { prompt } = require('inquirer');
+const { writeFile } = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -66,7 +67,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
+    writeFile(fileName, data, (err) =>
         err ? console.error(err) : console.log('Success!')
     );
 
@@ -74,7 +75,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
+    prompt(questions)
         .then((response) => {
             console.log(response);
             writeToFile('README.md', generateMarkdown(response));
